70481bfcdb6930553719213cff527c33dad96d47
Paul Canada <paul.canada0@gmail.com>
2018-06-10 13:13:39 -0400

    Add sys.excepthook for main program.

--- window_classes/MainWindow.py
+++ window_classes/MainWindow.py
@@ -1,7 +1,18 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QFrame
+from PyQt5.QtCore import pyqtSlot, pyqtSignal
 from gui_py.main_window_gui import Ui_MainWindow
+from window_classes.AddonWindow import AddonWindow
+from window_classes.SettingsWindow import SettingsWindow
 import sys
 from overrides.internal_overrides import MainWindowPrompt
+
+sys._excepthook = sys.excepthook
+
+def exception_hook(exctype, value, traceback):
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+
+sys.excepthook = exception_hook
 
 HANDLE_STYLE = """
 QSplitter::handle:horizontal {
@@ -21,6 +32,13 @@ background: #ccc;
 
 class MainWindow(MainWindowPrompt):
 
+    try:
+        OpenAddonAdder = pyqtSignal()
+        # settings_window = SettingsWindow()
+        # addon_window = AddonWindow()
+    except Exception as e:
+        print(e)
+
     def __init__(self):
         super(MainWindowPrompt, self).__init__()
 
@@ -28,7 +46,13 @@ class MainWindow(MainWindowPrompt):
         self.window.ui = Ui_MainWindow()
         self.window.ui.setupUi(self)
         self.window.ui.splitter.setStyleSheet(HANDLE_STYLE)
+        self.OpenAddonAdder.connect(self.add_addon)
+        self.window.ui.actionAddAddon.triggered.connect(self.OpenAddonAdder.emit)
 
+    @pyqtSlot()
+    def add_addon(self):
+        addon_window = AddonWindow()
+        addon_window.exec()
 
 def main():
     app = QApplication(sys.argv)
