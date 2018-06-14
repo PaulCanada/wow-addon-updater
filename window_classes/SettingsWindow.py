3353a1044b971bf5de19fa6f215f73a7aa3d7b02
PaulCanada <paul.canada0@gmail.com>
2018-06-14 11:42:55 -0400

    Add functionality to browse for WoW game directory
    This is used for setting the AddOns directory.

--- window_classes/SettingsWindow.py
+++ window_classes/SettingsWindow.py
@@ -1,5 +1,6 @@
 from gui_py.settings_gui import Ui_Settings
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog
+import logging
 
 
 class SettingsWindow(QDialog):
@@ -11,6 +12,7 @@ class SettingsWindow(QDialog):
         self.window.ui = Ui_Settings()
         self.window.ui.setupUi(self)
         self.parent = parent
+        self.window.ui.btnBrowseWowDirectory.clicked.connect(lambda: self.set_wow_dir(self.parent.settings))
 
         self.init_ui()
 
@@ -28,6 +30,33 @@ class SettingsWindow(QDialog):
     def ok_settings(self):
         self.save_settings()
         self.close()
+
+    def set_wow_dir(self, settings):
+        file_name = open_file_dialog("Browse for World of Warcraft Game Directory")
+
+        if file_name is not None:
+
+            if str(file_name).endswith("/AddOn"):
+                file_name.strip("/AddOn")
+
+            self.window.ui.leditWowDirectory.setText(file_name)
+            settings.data['settings']['wow_dir'] = file_name
+
+
+def open_file_dialog(title):
+    try:
+        options = QFileDialog.Options()
+
+        file_name = QFileDialog.getExistingDirectory(None, title, "", options=options)
+
+        if file_name != "":
+            print(file_name)
+            return file_name
+        else:
+            return None
+
+    except Exception as e:
+        logging.critical("Error opening File Dialog: {0}".format(e))
 
 
 def main():
