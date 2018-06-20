ea1ebe8634e71489e2cd6e1df12d081d3d8abae3
Paul Canada <paul.canada0@gmail.com>
2018-06-19 20:32:19 -0400

    Remove condition for checking if the folder exists.

--- UpdateChecker.py
+++ UpdateChecker.py
@@ -26,9 +26,7 @@ class UpdateChecker(object):
             logging.info("Retrieving latest version for addon: {0}".format(current_addon.name))
             current_addon.latest_version = current_addon.get_update_version()
 
-            if current_addon.current_version != current_addon.latest_version or not os.path.isdir(
-                    self.parent.settings.data['settings']['wow_dir'] + '/' + current_addon.name):
-
+            if current_addon.current_version != current_addon.latest_version:
                 logging.info("{0} is out of date!".format(self.parent.settings.data['addons'][key]['name']))
                 update_list.append(current_addon)
 
